8505410e3a2f27535b3a3c7d5353c6157dd657d4
gutoodc2 <gustavo.copini@gmail.com>
2017-11-22 12:35:14 -0200

    Telas Feitas]

---
+++ Abastecimento/app/src/main/java/gustavo/abastecimento/AdicionarAbastecimento.java
@@ -0,0 +1,13 @@
+package gustavo.abastecimento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionarAbastecimento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_abastecimento);
+    }
+}
---
+++ Abastecimento/app/src/main/java/gustavo/abastecimento/MainActivity.java
@@ -0,0 +1,13 @@
+package gustavo.abastecimento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_abastecimento);
+    }
+}
